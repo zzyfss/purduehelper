5bb10ef28e50ebce1cf0a3734f8e838f45474511
zzyfss <zzyfss@163.com>
2013-03-02 00:27:42 -0500

    fixed server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ Meteor.publish("directory", function () {
   return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
 });
 
-Meteor.publish("events", function () {
+Meteor.publish("parties", function () {
   return Events.find(
     {$or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]});
 });
